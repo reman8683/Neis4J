59e73f73880347321674b5e26fe61754792e7dbf
REMAN <reman8683@gmail.com>
2023-07-13 03:09:07 +0900

    수행시간 테스트

--- src/main/java/com/reman8683/neis4j/Main.java
+++ src/main/java/com/reman8683/neis4j/Main.java
@@ -5,14 +5,24 @@ import com.reman8683.neis4j.dto.Meal;
 import com.reman8683.neis4j.dto.School;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        School school = new School("정관고등학교");
-        Meal meal = new MealInfo().getMeal(school, "20230523");
-        meal.removeAllergy();
-        System.out.println(meal.getMenu());
-        System.out.println(school);
+        long avg = 0;
+        int count = 25;
+        for (int i = 0; i < count; i++) {
+            Instant start = Instant.now();
+            ////////
+            School school = new School("정관고등학교");
+            Meal meal = new MealInfo().getMeal(school, "20230523");
+            meal.removeAllergy();
+            ////////
+            Instant end = Instant.now();
+            avg += Duration.between(start, end).toMillis();
+        }
+        System.out.println("수행시간 평균: " + (float) avg / count  + " ms");
     }
 }
